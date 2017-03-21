e661b147d9210ffe462bce043c4978d6f465d31e
Ubuntu <ubuntu@ip-172-31-19-195.us-west-2.compute.internal>
2017-03-21 23:00:40 +0000

    add logback.xml; copy into Docker image; add worker_tags.json
    add logback.xml to control size of logfile container_provisioner_nohup.out
    add worker_tags.json to tag worker instances
    fix path to get logback.xml before finding it in jar file

--- consonance/init_provisioner.sh
+++ consonance/init_provisioner.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+#we put the logback.xml file in /root; so put that path first so the provisioner
+#uses those logging parameters instead of using the ones in consonance-arch-*.jar
+#which is in /
+#the '/' path is second so that the rest of the code is found in 
+#consonance-arch-*.jar
+java -cp "/root/:./*" io.consonance.arch.containerProvisioner.ContainerProvisionerThreads --config config --endless | tee /consonance_logs/container_provisioner_nohup.out
 
-java -cp consonance-arch-*.jar io.consonance.arch.containerProvisioner.ContainerProvisionerThreads --config config --endless | tee /consonance_logs/container_provisioner_nohup.out
+#java -cp consonance-arch-*.jar io.consonance.arch.containerProvisioner.ContainerProvisionerThreads --config config --endless | tee /consonance_logs/container_provisioner_nohup.out

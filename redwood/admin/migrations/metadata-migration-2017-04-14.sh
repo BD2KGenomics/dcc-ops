8fc310f71c418db927a799ebd5a572a977195df5
Benjamin Ran <benjaminran2@gmail.com>
2017-04-18 18:03:38 -0700

    separate migration wizard from redwood recover, migration to UNRESOLVED project

--- redwood/admin/migrations/metadata-migration-2017-04-14.sh
+++ redwood/admin/migrations/metadata-migration-2017-04-14.sh
@@ -3,10 +3,18 @@ set -e
 
 #"${_REDWOOD_ROOT}/bin/redwood" project create TREEHOUSE SU2C PROTECT_NBL
 
-docker exec -i redwood-metadata-db mongo --norc --quiet dcc-metadata <<EOF
+docker exec -i redwood-metadata-db mongo --norc --quiet <<EOF
 conn = new Mongo();
 db = conn.getDB("dcc-metadata");
-db.Entity.updateMany({}, {$set:{access:"controlled"}}, {})
-db.Entity.updateMany({}, {$set:{projectCode:"DEV"}}, {})
+db.Entity.update(
+    {},
+    { \$set:
+      {
+          access:"controlled",
+          projectCode:"UNRESOLVED"
+      }
+    },
+    { multi: true }
+)
 EOF
 echo done

3b54694e12fa841b88acae4e53acb1bcbbb613a4
Benjamin Ran <benjaminran2@gmail.com>
2017-04-27 12:50:03 -0700

    bundle program mapping and redwood migration

--- redwood/admin/migrations/metadata-migration-2017-04-14.sh
+++ redwood/admin/migrations/metadata-migration-2017-04-14.sh
@@ -1,9 +1,30 @@
 #!/usr/bin/env bash
 set -e
 
-#"${_REDWOOD_ROOT}/bin/redwood" project create TREEHOUSE SU2C PROTECT_NBL
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+mapping="${dir}/helper/mapping.csv"
 
-docker exec -i redwood-metadata-db mongo --norc --quiet <<EOF
+# create projects
+if [[ -z ${_REDWOOD_ROOT} ]]; then
+    echo 'not running within redwood cli: you need to manually run `redwood project create TREEHOUSE SU2C PROTECT_NBL`'
+else
+    echo "adding projects"
+    "${_REDWOOD_ROOT}/bin/redwood" project create TREEHOUSE SU2C PROTECT_NBL
+fi
+
+function mapping_for() {
+    program="$1"
+    printf '["'; cat "${mapping}" | grep "${program}" | cut -d, -f 1 | tr "\n" ',' | sed 's/,$//' | sed 's/,/","/g' | tr -d '\n'; printf '"]'
+}
+
+# json arrays of bundle_ids
+treehouse_bundles="$(mapping_for Treehouse)"
+wcdt_bundles="$(mapping_for SU2C)"
+protect_bundles="$(mapping_for PROTECT_NBL)"
+
+# write migration.js
+tmpfile="/tmp/migration`date +%Y-%m-%d_%H-%M-%S`.js"
+cat >"${tmpfile}" <<EOF
 conn = new Mongo();
 db = conn.getDB("dcc-metadata");
 db.Entity.update(
@@ -15,6 +36,56 @@ db.Entity.update(
       }
     },
     { multi: true }
-)
+);
+db.Entity.bulkWrite([
+    { updateMany:
+      {
+          "filter": {
+              gnosId: {
+                  \$in: ${treehouse_bundles}
+              }
+          },
+          "update": {
+              \$set: {
+                  projectCode:"Treehouse"
+              }
+          }
+      }
+    },
+    { updateMany:
+      {
+          "filter": {
+              gnosId: {
+                  \$in: ${wcdt_bundles}
+              }
+          },
+          "update": {
+              \$set: {
+                  projectCode:"SU2C"
+              }
+          }
+      }
+    },
+    { updateMany:
+      {
+          "filter": {
+              gnosId: {
+                  \$in: ${protect_bundles}
+              }
+          },
+          "update": {
+              \$set: {
+                  projectCode:"PROTECT_NBL"
+              }
+          }
+      }
+    }
+])
 EOF
-echo done
+
+echo "running mongodb migration script ${tmpfile}"
+docker cp "${tmpfile}" redwood-metadata-db:"${tmpfile}"
+docker exec -i redwood-metadata-db mongo --norc --quiet "${tmpfile}"
+
+echo done!
+echo '`echo "DBQuery.shellBatchSize = 10000; db.Entity.find({projectCode:\"UNRESOLVED\"})" | docker exec -it redwood-metadata-db mongo dcc-metadata` to see unresolved bundles. You still need to resolve these bundle programs manually.'

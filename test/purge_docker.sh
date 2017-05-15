136065789809df8f50a9a0a7bd57fe2614994328
caaespin <caaespin@ucsc.edu>
2017-05-15 21:49:16 +0000

    Adding boto config template and requirements to the provisioner ; Onboarding Walt's script to refresh worker instances; updated install bootstrap; added line to purge docker script; added cron job line on the init provisioner script

--- test/purge_docker.sh
+++ test/purge_docker.sh
@@ -2,6 +2,7 @@
 
 docker kill $(docker ps -a -q)
 docker rmi $(docker images -a -q)
+docker rmi $(docker images -q -f dangling=true)
 docker rm $(docker ps -a -f status=exited -q)
 docker volume rm $(docker volume ls -f dangling=true -q)
 

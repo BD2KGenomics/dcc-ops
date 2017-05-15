f1f4f67745f3202d47dac5e002a7e8e3a2d17a14
caaespin <caaespin@ucsc.edu>
2017-05-15 01:26:24 +0000

    Adding walt's script. Still needs to be tested. Not implemented until further testing

--- consonance/init_provisioner.sh
+++ consonance/init_provisioner.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+#cron -f -L 15 && echo "Cron Job set"
+
 java -cp consonance-arch-*.jar io.consonance.arch.containerProvisioner.ContainerProvisionerThreads --config config --endless | tee /consonance_logs/container_provisioner_nohup.out

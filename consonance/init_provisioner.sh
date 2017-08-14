82041b81d9cae7f68dd7cc49c251528071922498
Walt Shands <jshands@ucsc.edu>
2017-08-14 19:22:10 +0000

    fix command line
    fix command line

--- consonance/init_provisioner.sh
+++ consonance/init_provisioner.sh
@@ -10,4 +10,7 @@ cron -L 15 && echo "Cron Job set"
 #java -agentlib:jdwp=transport=dt_socket,server=y,address=8000,suspend=n  -cp "/root/:./*" io.consonance.arch.containerProvisioner.ContainerProvisionerThreads --config config --endless | tee /consonance_logs/container_provisioner_nohup.out
 
 #regular run command
-java -cp consonance-arch-*.jar io.consonance.arch.containerProvisioner.ContainerProvisionerThreads --config config --endless | tee /consonance_logs/container_provisioner_nohup.out
+java  -cp "/root/:./*" io.consonance.arch.containerProvisioner.ContainerProvisionerThreads --config config --endless | tee /consonance_logs/container_provisioner_nohup.out
+
+#old command
+#java -cp consonance-arch-*.jar io.consonance.arch.containerProvisioner.ContainerProvisionerThreads --config config --endless | tee /consonance_logs/container_provisioner_nohup.out
